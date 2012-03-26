568b4781ce54ee4acda59ca99850d6b70d1000b9
unknown <y-yamamoto@y-yamamoto-PC.(none)>
2012-03-26 14:42:37 +0900

    first comit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+﻿var $teki = $('IMG.fade[alt="敵襲"]');
+if ($teki.get().length>0) {
+	// 敵襲あり
+	/* content script */
+	chrome.extension.sendRequest({greeting: "hello"}, function(response) {
+	  console.log();
+	});
+}
